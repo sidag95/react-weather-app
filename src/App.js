1ffc3d3df36dfc16c0a61e8c82c663650b229d70
siddhant <siddhant@systeminsights.com>
2018-12-24 22:31:46 +0530

    starting up our project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">Siddhant Agarwal</header>
+        <p>This is our first react application. Woohoo!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
