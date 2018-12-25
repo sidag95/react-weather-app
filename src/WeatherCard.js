342daafbc0418cda59071d9571730d56c463dc50
siddhant <siddhant@systeminsights.com>
2018-12-25 13:21:21 +0530

    styling

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -1,13 +1,20 @@
 import * as React from "react";
 import "./WeatherCard.css";
 
+const weatherToClassMap = {
+  Sunny: "sunny",
+  Rainy: "rainy",
+  Overcast: "overcast",
+  Cloudy: "cloudy"
+}
+
 function WeatherCard(props) {
   const {day, weather, currentDay, showFullScreen, onClick, onClose} = props;
   console.log(showFullScreen)
   const isCurrentDay = day === currentDay;
   return (
     <div 
-      className={`card ${isCurrentDay ? "active" : ""} ${showFullScreen ? "fullScreen" : ""}`}
+      className={`card ${weatherToClassMap[weather]} ${isCurrentDay ? "active" : ""} ${showFullScreen ? "fullScreen" : ""}`}
       onClick = {_ => onClick ? onClick({day, weather}) : null}
     >
       <div>{day}</div>
